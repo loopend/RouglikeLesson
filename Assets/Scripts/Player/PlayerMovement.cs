09f63e139828ce655ea36b06f6b9d0af543bb02c
Karasin Denis <karasindenis@users.noreply.github.com>
2026-04-20 00:55:19 +0500

    AnimationControler

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]private float _moveSpeed;
+    [SerializeField] private Animator _animator;
+    private Vector3 _movement;
+    public Vector3 Movement => _movement;
+
+    private void Update() => Move();
+
+
+    private void Move()
+    {
+        _movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        transform.position += _movement.normalized * (_moveSpeed * Time.deltaTime);
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Speed", _movement.sqrMagnitude);
+    }
+
+
+
+
+}
