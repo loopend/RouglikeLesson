a3f5b8f36030913388fee04185016488c16bf4dc
Karasin Denis <95399237+loopend@users.noreply.github.com>
2026-08-02 13:30:39 +0500

    M12_Bomb

--- Assets/Scripts/GameCore/Loot/Bomb.cs
+++ Assets/Scripts/GameCore/Loot/Bomb.cs
@@ -8,17 +8,31 @@ namespace Assets.Scripts.GameCore.Loot
 {
     public class Bomb : MonoBehaviour
     {
+        private static readonly int Activate = Animator.StringToHash("Activate");
+
         [SerializeField] private float _explosionDelay = 3f;
         [SerializeField] private float _explosionRadius = 5f;
         [SerializeField] private AudioClip _explosionSound;
         [SerializeField] [Range(0f, 1f)] private float _explosionSoundVolume = 1f;
+        [SerializeField] private Animator _animator;
 
         private ParticleBombSpawner _particleBombSpawner;
         private Coroutine _explosionRoutine;
+        private bool _isArmed;
+
+        private void Awake()
+        {
+            if (_animator == null)
+                _animator = GetComponentInChildren<Animator>();
+        }
 
         private void OnEnable()
         {
-            _explosionRoutine = StartCoroutine(ExplosionRoutine());
+            _isArmed = false;
+            _explosionRoutine = null;
+
+            if (_animator != null)
+                _animator.Play("BombIdle", 0, 0f);
         }
 
         private void OnDisable()
@@ -28,6 +42,24 @@ namespace Assets.Scripts.GameCore.Loot
                 StopCoroutine(_explosionRoutine);
                 _explosionRoutine = null;
             }
+
+            _isArmed = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (_isArmed)
+                return;
+
+            if (!other.TryGetComponent(out PlayerHealth _))
+                return;
+
+            _isArmed = true;
+
+            if (_animator != null)
+                _animator.SetTrigger(Activate);
+
+            _explosionRoutine = StartCoroutine(ExplosionRoutine());
         }
 
         private IEnumerator ExplosionRoutine()
