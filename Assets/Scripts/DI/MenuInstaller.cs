30eddc456474125c50f760d06e96247297a64a11
Karasin Denis <95399237+loopend@users.noreply.github.com>
2026-08-09 19:15:31 +0500

    M13_Shop

---
+++ Assets/Scripts/DI/MenuInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Menu;
+using UnityEngine;
+using Zenject;
+using Assets.Scripts.Enemy;
+using Assets.Scripts.GameCore;
+using Assets.Scripts.GameCore.ExperienceSystem;
+using Assets.Scripts.GameCore.LevelSystem;
+using Assets.Scripts.GameCore.Loot;
+using Assets.Scripts.GameCore.Pause;
+using Assets.Scripts.GameCore.UI;
+using Assets.Scripts.GameCore.UpgradeSystem;
+
+namespace Assets.Scripts.DI
+{
+    public class MenuInstaller : MonoInstaller
+    {
+        [SerializeField] private MenuUIUpdater _menuUIUpdater;
+        public override void InstallBindings()
+        {
+            Container.Bind<MenuUIUpdater>().FromInstance(_menuUIUpdater);
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,53 @@
+﻿using Assets.Scripts.ScenesLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Assets.Scripts.Menu
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [SerializeField] private Button _startButton;
+        [SerializeField] private Button _shopButton;
+        [SerializeField] private Button _closeButton;
+        [SerializeField] private GameObject _shopWindow;
+
+        private SceneLoader _sceneLoader;
+        private MenuUIUpdater _menuUIUpdater;
+
+        private void OnEnable()
+        {
+            _startButton.onClick.AddListener(_sceneLoader.Game);
+            _shopButton.onClick.AddListener(OpenShopWindow);
+            _closeButton.onClick.AddListener(Application.Quit);
+        }
+
+        private void OnDisable()
+        {
+            _startButton.onClick.RemoveListener(_sceneLoader.Game);
+            _shopButton.onClick.RemoveListener(OpenShopWindow);
+            _closeButton.onClick.RemoveListener(Application.Quit);
+        }
+         private void OpenShopWindow()
+        {
+            _shopWindow.SetActive(true);
+            _menuUIUpdater.UpdateUI();
+        }
+         private void StartGame()
+        {
+            _sceneLoader.Game();
+        }
+
+        [Inject]
+        private void Construct(SceneLoader sceneLoader, MenuUIUpdater menuUIUpdater)
+        {
+            _sceneLoader = sceneLoader;
+            _menuUIUpdater = menuUIUpdater;
+        }
+    }
+}
